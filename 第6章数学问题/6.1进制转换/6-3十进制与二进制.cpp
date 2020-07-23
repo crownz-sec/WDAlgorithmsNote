681c7c8bb1f993ad0eae9efbd8e0bc5d2fd2bf44
Xiwen Lu <1159706522@qq.com>
2020-07-23 15:14:33 +0800

    恢复一些个人写的

--- 第6章数学问题/6.1进制转换/6-3十进制与二进制.cpp
+++ 第6章数学问题/6.1进制转换/6-3十进制与二进制.cpp
@@ -1,72 +1,68 @@
 #include<iostream>
-#include<string>
+#include<algorithm>
+#include<string.h>
 #include<vector>
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 6dd9bdd... æ›´æ–°6-2ï¼Œ6-3
 using namespace std;
-/**
-´ËÌâÊı×ÖÎª1000Î»£¬ÆÕÍ¨ÕûĞÍÎŞ·¨´æ´¢
-**/
+/**Ê®½øÖÆÓë¶ş½øÖÆ 
+*Ë¼Â·£ºÊ®½øÖÆ×ª¶ş½øÖÆ£¬¶ş½øÖÆÄæĞò£¬¶ş½øÖÆ×ªÊ®½øÖÆ£¨³ËÒ»¸ö²»Í¬µÄ10Ö¸ÊıºóÏà¼Ó£© 
+*×¢Òâ£º´ËÌâÊı¾İÈÔ½Ï´ó£¬1000Î»ÒÔÄÚ 
+*/
 
-//×Ö·û´®³ı·¨
-string divide(string str,int x) {
-    int remainder=0;//±£ÁôÓàÊı
-    for(int i=0; i<str.size(); i++) {
-        int current=str[i]-'0'+remainder*10;
-        str[i]=current/x+'0';
-        remainder=current%x;
-    }
-    int index=0;
-    while(str[index]=='0')index++;
-    return str.substr(index);
+string divide2(const string &s){
+	//Ä£ÄâÊÖ¶¯³ıÒÔ2
+	const int n=s.size();
+	string res(n,'0');
+	int bias=0;
+	for(int i=0;i<n;++i){
+		res[i]=(s[i]-'0'+10*bias)/2+'0';
+		bias=(s[i]-'0'+10*bias)%2;
+	}
+	int index=0;
+	while(res[index]=='0')++index;
+	return res.substr(index);
 }
 
-//×Ö·û´®³Ë·¨
-string mutiple(string str,int x) {
-    int carry=0;//±£´æ½øÎ»
-    for(int i=str.size()-1; i>=0; i--) {
-        int current=x*(str[i]-'0')+carry;
-        str[i]=current%10+'0';
-        carry=current/10;
-    }
-    if(carry!=0) {
-        str="1"+str;
-    }
-    return str;
+string multiple(string str,const int& x,int carry){
+	carry=0;//´æ´¢½øÎ»
+	for(int i=str.size()-1;i>=0;--i){
+		int curr=x*(str[i]-'0')+carry;
+		str[i]=curr%10+'0';
+		carry = curr/10;
+	} 
+	if(carry!=0)str=(char)('0'+carry)+str;
+	return str;
 }
 
-//×Ö·û´®¼Ó·¨
-string add(string str,int x) {
-    int carry=x;
-    for(int i=str.size()-1; i>=0; i--) {
-        int current=(str[i]-'0')+carry;
-        str[i]=current%10+'0';
-        carry=current/10;
-    }
-    if(carry!=0) {
-        str="1"+str;
-    }
-    return str;
+string add(string str,int x){
+	return multiple(str,1,x);
 }
 
-int main() {
-    string str;
-    while(getline(cin,str)) {
-        vector<int> binary;
-        while(str.size()!=0) {
-            //×îµÍÎ»¼ÆËã
-            int last=str[str.size()-1]-'0';
-            //È¡Ä£ÔËËã
-            binary.push_back(last%2);
-            //Õû³ıÔËËã
-            str=divide(str,2);
-        }
-        string answer="0";
-        for(int i=0; i<binary.size(); i++) {
-            answer=mutiple(answer,2);//³Ë·¨ÔËËã
-            answer=add(answer,binary[i]);//¼Ó·¨ÔËËã
-        }
-        cout<<answer<<endl;
-    }
 
-    return 0;
+int main(){
+	vector<int> v;
+	string A;
+	int last;
+	while(cin>>A){
+		while(A.size()!=0){
+			last = A[A.size()-1]-'0';
+			v.push_back(last%2);
+			A = divide2(A);	
+		}
+		string ans = "0";
+		for(int i=0;i<v.size();++i){
+			ans = multiple(ans,2,0);
+			ans = add(ans,v[i]);
+			cout<<ans<<endl;
+		} 
+		cout<<ans<<endl;
+		
+	}
+	//vµÄÕıĞò¼´Ô­Êı¶ş½øÖÆÄæĞò
+
+	return 0;
 }
+
